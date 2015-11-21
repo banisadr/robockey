26fd0de28fdfeaf59cd4c36d942c7f57a8bc4953
banisadr <bahram.banisadr@gamil.com>
2015-11-21 18:41:46 -0500

    Motor controller debugged. Gains and mixing need tuning

--- motor_controler/motor_controler/Debug/makedep.mk
+++ motor_controler/motor_controler/Debug/makedep.mk
@@ -2,6 +2,8 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
+localization_function.c
+
 main.c
 
 m_bus.c
--- motor_controler/motor_controler/main.c
+++ motor_controler/motor_controler/main.c
@@ -14,7 +14,8 @@ Header Information
 /*****************************************************************
 ==================================================================
 ### INTERNAL README: ADD TO EVERYTIME BEFORE SENDING TO GITHUB ###
-Modified by Bahram on 11/18/2015 go to given X Y location
+Modified by Bahram on 11/18/2015 go to given X Y location (errors)
+Modified by All on 11/18/2015 basic linear & angle control working
 ==================================================================
 *****************************************************************/
 
@@ -27,6 +28,8 @@ Included Files & Libraries
 #include "m_bus.h"
 #include "m_rf.h"
 #include "m_usb.h"
+#include "m_wii.h"
+#include "localization_function.h"
 
 /************************************************************
 Definitions
@@ -45,10 +48,10 @@ Definitions
 #define PWM_FREQ 1000
 
 /* PD Controller Values */
-#define THETA_Kp 2.33
-#define THETA_Kd 0.01
-#define LINEAR_Kp 0.5
-#define LINEAR_Kd 0.1
+#define THETA_Kp 1.00
+#define THETA_Kd 0.00
+#define LINEAR_Kp 1.00
+#define LINEAR_Kd 0.00
 
 /* Other */
 #define PI 3.14159265359
@@ -79,13 +82,18 @@ float right_duty_cycle = 0.1;
 float x = 0;
 float y = 0;
 float theta = 0;
-float x_target = -300;
+float x_target = 0;
 float y_target = 0;
-float theta_target = PI;
+float theta_target = 0;
 
 /* PD Controler Values */
 float previous_theta_error = 0;
 float previous_linear_error = 0;
+
+/* Wii Camera */
+unsigned int blobs[12];
+float robotCenterPrev[3] = {1023, 1023, 360};
+float* robotCenter;
 
 /************************************************************
 Main Loop
@@ -97,7 +105,7 @@ int main(void)
 
 	/* Initializations */
 	init();
-	//usb_enable();
+	usb_enable();
 	timer1_init();
 	timer3_init();
 
@@ -122,7 +130,6 @@ void init(void){
 	//Set to Input
 	clear(DDRD,0); // D0
 	clear(DDRD,1); // D1
-	clear(DDRD,2); // D2
 	
 	
 	//Set to Output
@@ -138,6 +145,7 @@ void init(void){
 	clear(PORTB,3);
 	
 	m_bus_init();
+	while(!m_wii_open());
 	sei();
 }
 
@@ -198,18 +206,37 @@ int lowpass(float alpha, int previous_output, int reading)
 /* Get x, y, theta, filterupdate control */
 void update_position(void)
 {
-
-	#TODO: location = [x, y, theta]
+	// Read in Locations
+	m_wii_read(blobs);
+	unsigned int x1 = blobs[0];
+	unsigned int y1 = blobs[1];
+	unsigned int x2 = blobs[3];
+	unsigned int y2 = blobs[4];
+	unsigned int x3 = blobs[6];
+	unsigned int y3 = blobs[7];
+	unsigned int x4 = blobs[9];
+	unsigned int y4 = blobs[10];
 	
-	x = lowpass(ALPHA_LOW, x, location[0]);
-	y = lowpass(ALPHA_LOW, y, location[1]);
-	theta = lowpass(ALPHA_LOW, theta, location[2]);
+	int rawStarData[8] = {x1, x2, x3, x4, y1, y2, y3, y4};
+	robotCenter = localization_calc(rawStarData, robotCenterPrev);
+	
+	robotCenterPrev[2] = robotCenter[2];
+	
+	// Correct for offset discontinuity
+	robotCenter[2] = theta_error_correction(robotCenter[2]);
+	
+	x = lowpass(ALPHA_LOW, x, robotCenter[0]);
+	y = lowpass(ALPHA_LOW, y, robotCenter[1]);
+	theta = lowpass(ALPHA_LOW, theta, robotCenter[2]);
 	
 	if (check(TIFR3,OCF3A)){	// Check if timestep has completed
 		set(TIFR3,OCF3A);		// Reset flag
 		theta_target = atan2(y_target-y,x_target-x); // Update theta target
 		run_control_loop(); // Update control
 	}
+	
+	robotCenterPrev[0] = x;
+	robotCenterPrev[1] = y;
 }
 
 /* PID Control (Currently only for theta) */
@@ -219,61 +246,65 @@ void run_control_loop(void)
 	float theta_error = theta_target - theta;
 	theta_error = theta_error_correction(theta_error);
 	float derivative = (theta_error-previous_theta_error)/TIMESTEP;
-	float output = THETA_Kp*theta_error - THETA_Kd*derivative; //If output > 0, turn left
+	float angular_output = THETA_Kp*theta_error - THETA_Kd*derivative; //If output > 0, turn left
 	previous_theta_error = theta_error;
-	output = output/PI; //Normalize to max value of 1 given Kp gain of 1
+	angular_output = angular_output/PI; //Normalize to max value of 1 given Kp gain of 1
 	
 	// Update duty cycle based on angle
-	if (output > 0){
-		left_duty_cycle = - output;
-		right_duty_cycle = output;
-	} else{
-		left_duty_cycle = output;
-		right_duty_cycle = -output;
-	}
+	left_duty_cycle = - angular_output;
+	right_duty_cycle = angular_output;
 	
 	// Get linear error based output
 	float y_delta = y_target-y;
 	float x_delta = x_target-x;
-	float linear_error = cos(abs(theta_error))*sqrt(y_delta*y_delta+x_delta*x_delta); // Gets component of translational error in direction bot is facing
+	float linear_error = fabs(cos(theta_error))*sqrt(y_delta*y_delta+x_delta*x_delta); // Gets component of translational error in direction bot is facing
 	derivative = (linear_error-previous_linear_error)/TIMESTEP;
-	output = LINEAR_Kp*linear_error - LINEAR_Kd*derivative;
+	float linear_output = LINEAR_Kp*linear_error - LINEAR_Kd*derivative;
 	previous_linear_error = linear_error;
-	output = output/40; //Normalize to value of 1 at 40 pixles (~10 cm) given Kp gain of 1
+	if(linear_output>40.0){linear_output=40.0;}
+	linear_output = linear_output/40; //Normalize to value of 1 at 40 pixels (~10 cm) given Kp gain of 1
 	
 	// Update duty cycle based on linear distance
-	if (abs(theta_error) < PI/2){
-		left_duty_cycle += output;
-		right_duty_cycle += output;
-		} else{
-		left_duty_cycle -= output;
-		right_duty_cycle -= output;
-	}
+// 	if (fabs(theta_error) < PI/2){
+// 		left_duty_cycle += linear_output;
+// 		right_duty_cycle += linear_output;
+// 		} else{
+// 		left_duty_cycle -= linear_output;
+// 		right_duty_cycle -= linear_output;
+// 	}
 	
 	// Set motor direction based on pos/neg
-	if (left_duty_cycle<0){set(PORTB,1);}
-	else{clear(PORTB,1);}
+	if (left_duty_cycle<0){clear(PORTB,1);}
+	else{set(PORTB,1);}
 		
-	if (right_duty_cycle<0){set(PORTB,3);}
-	else{clear(PORTB,3);}
-	
+	if (right_duty_cycle<0){clear(PORTB,3);}
+	else{set(PORTB,3);}
 	
 	// Set duty cycle
-	left_duty_cycle = abs(left_duty_cycle)/(PI);
-	if (left_duty_cycle > 1) {left_duty_cycle = 1;}
+	left_duty_cycle = fabs(left_duty_cycle);
+	right_duty_cycle = fabs(right_duty_cycle);
 	
-	right_duty_cycle = abs(right_duty_cycle)/(PI);
-	if (right_duty_cycle > 1) {	right_duty_cycle = 1;}
-	
+	while (left_duty_cycle > 0.2)
+	{
+		left_duty_cycle = 0.9*left_duty_cycle;
+		right_duty_cycle = 0.9*right_duty_cycle;
+	}
+
+	while (right_duty_cycle > 0.2)
+	{
+		left_duty_cycle = 0.9*left_duty_cycle;
+		right_duty_cycle = 0.9*right_duty_cycle;
+	}
 	// Update timer values
-	OCR1B = (float)OCR1A*left_duty_cycle;
-	OCR1C = (float)OCR1A*right_duty_cycle;
+	OCR1B = ((float)OCR1A)*left_duty_cycle;
+	OCR1C = ((float)OCR1A)*right_duty_cycle;
+
 }
 
 /* Ensures that bot doesn't turn more that PI in search of theta_target */
 float theta_error_correction(float error)
 {
-	if(abs(error)>PI){return error-2.0*PI*error/abs(error);}
+	if(fabs(error)>PI){return error-2.0*PI*error/fabs(error);}
 	else{return error;}
 }
 
